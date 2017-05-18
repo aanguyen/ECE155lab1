800c404b1b9375edb0cb9dae4123324b2f3beb92
Andrew Nguyen <andrew.nguyen.waterloo@gmail.com>
2017-05-18 19:17:13 -0400

    Added fields for max values

--- Lab1_202_08/app/src/main/java/lab1_202_08/uwaterloo/ca/lab1_202_08/Lab1_202_08.java
+++ Lab1_202_08/app/src/main/java/lab1_202_08/uwaterloo/ca/lab1_202_08/Lab1_202_08.java
@@ -22,104 +22,132 @@ public class Lab1_202_08 extends AppCompatActivity {
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         TextView lightText = (TextView) findViewById(R.id.lightFieldText);
+        TextView lightMax = (TextView) findViewById(R.id.lightFieldMaxText);
         Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        SensorEventListener light = new LightSensorEventListener(lightText);
+        SensorEventListener light = new LightSensorEventListener(lightText, lightMax);
         sensorManager.registerListener(light, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-        TextView magneticFieldText = (TextView) findViewById(R.id.magneticFieldText);
-        Sensor magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        SensorEventListener magneticField = new MagneticSensorEventListener(magneticFieldText);
-        sensorManager.registerListener(magneticField, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        TextView accelerometerText = (TextView) findViewById(R.id.accelerometerLabel);
+        TextView accelerometerText = (TextView) findViewById(R.id.accelerometerText);
+        TextView accelerometerMax = (TextView) findViewById(R.id.accelerometerMaxText);
         Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        SensorEventListener accelerometer = new AccelerometerSensorEventListener(accelerometerText);
+        SensorEventListener accelerometer = new AccelerometerSensorEventListener(accelerometerText, accelerometerMax);
         sensorManager.registerListener(accelerometer, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-        TextView rotationVectorText = (TextView) findViewById(R.id.rotationVectorLabel1);
+        TextView magneticFieldText = (TextView) findViewById(R.id.magneticFieldText);
+        TextView magneticFieldMax = (TextView) findViewById (R.id.magneticFieldMaxText);
+        Sensor magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        SensorEventListener magneticField = new MagneticSensorEventListener(magneticFieldText, magneticFieldMax);
+        sensorManager.registerListener(magneticField, magneticFieldSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        TextView rotationVectorText = (TextView) findViewById(R.id.rotationVectorText);
+        TextView rotationVectorMax = (TextView) findViewById(R.id.rotationVectorMaxText);
         Sensor rotationVectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        SensorEventListener rotationVector = new RotataionVectorSensorEventListener(rotationVectorText);
+        SensorEventListener rotationVector = new RotataionVectorSensorEventListener(rotationVectorText, rotationVectorMax);
         sensorManager.registerListener(rotationVector, rotationVectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
 }
 
-class MagneticSensorEventListener implements SensorEventListener {
-    private TextView output;
-
-    public MagneticSensorEventListener(TextView outputView){
-        output = outputView;
-    }
-    public void onAccuracyChanged(Sensor s, int i){}
-
-    public void onSensorChanged(SensorEvent se) {
-        if (se.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
-            float magneticFieldValue1 = se.values[0];
-            float magneticFieldValue2 = se.values[1];
-            float magneticFieldValue3 = se.values[2];
-            output.setText("The Magnetic Sensor Reading is: \n(" + String.format("%.2f", se.values[0]) + ", " + String.format("%.2f", se.values[1]) + ", " + String.format("%.2f", se.values[2]) + ")");
-        }
-    }
-}
-
+//Note: All se.values are given to us by Android OS as floats
 class LightSensorEventListener implements SensorEventListener {
     private TextView output;
+    private TextView outMax;
+    private float maxLight = 0.0f;
+    //do we have to initialize these TextViews to null?
 
-    public LightSensorEventListener(TextView outputView){
-
+    public LightSensorEventListener(TextView outputView, TextView outputMax){
+        outMax = outputMax;
         output = outputView;
     }
-    public void onAccuracyChanged(Sensor s, int i){}
+    public void onAccuracyChanged(Sensor s, int i){
+
+    }
 
     public void onSensorChanged(SensorEvent se) {
         if (se.sensor.getType() == Sensor.TYPE_LIGHT){
-            float lightValue = se.values[0];
-            String lightMessage = "The Light Sensor Reading is:\n" +  String.valueOf(lightValue);
-            output.setText(lightMessage);
+            output.setText("The Light Sensor Reading is:\n" +  String.valueOf(se.values[0]));
+            if (se.values[0] > maxLight) {
+                maxLight = se.values[0];
+                outMax.setText("The Highest Light Sensor Reading is:\n" + String.valueOf(maxLight));
+            }
         }
     }
 }
 
 class AccelerometerSensorEventListener implements SensorEventListener {
     private TextView output;
+    private TextView outMax;
+    private float maxX = -999.9f;
+    private float maxY = -999.9f;
+    private float maxZ = -999.9f;
 
-    public AccelerometerSensorEventListener(TextView outputView) {
-
+    public AccelerometerSensorEventListener(TextView outputView, TextView outputMax) {
         output = outputView;
+        outMax = outputMax;
+    }
+
+    public void onAccuracyChanged(Sensor s, int i) {
 
     }
 
-    public void onAccuracyChanged(Sensor s, int i){}
-
     public void onSensorChanged(SensorEvent se) {
         if (se.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            //float xValue = se.values[0];
-            //float yValue = se.values[1];
-            //float zValue = se.values[2];
-            String accelerometerMessage = "The Accelerometer Sensor Reading is:\n(" +  (String.format("%.2f", se.values[0])) + ", " + (String.format("%.2f", se.values[1])) + ", " + (String.format("%.2f", se.values[1])) + ")";
-            output.setText(accelerometerMessage);
+            output.setText("The Accelerometer Sensor Reading is:\n(" +  (String.format("%.2f", se.values[0])) + ", " + (String.format("%.2f", se.values[1])) + ", " + (String.format("%.2f", se.values[2])) + ")");
+            if (se.values[0] > maxX || se.values[1] > maxY || se.values[2] > maxZ) {
+                //code to change outMax here
+                //the condition inside if loop might be wrong
+            }
+        }
+    }
+}
+
+class MagneticSensorEventListener implements SensorEventListener {
+    private TextView output;
+    private TextView outMax;
+    private float maxX = -999.9f;
+    private float maxY = -999.9f;
+    private float maxZ = -999.9f;
+
+    public MagneticSensorEventListener(TextView outputView, TextView outputMax){
+        output = outputView;
+        outMax = outputMax;
+    }
+    public void onAccuracyChanged(Sensor s, int i) {
+
+    }
+
+    public void onSensorChanged(SensorEvent se) {
+        if (se.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+            output.setText("The Magnetic Sensor Reading is: \n(" + String.format("%.2f", se.values[0]) + ", " + String.format("%.2f", se.values[1]) + ", " + String.format("%.2f", se.values[2]) + ")");
+            if (se.values[0] > maxX || se.values[1] > maxY || se.values[2] > maxZ) {
+                //same as above
+            }
         }
     }
 }
 
 class RotataionVectorSensorEventListener implements SensorEventListener {
     private TextView output;
+    private TextView outMax;
+    private float maxX = -999.9f;
+    private float maxY = -999.9f;
+    private float maxZ = -999.9f;
 
-    public RotataionVectorSensorEventListener(TextView outputView) {
-
+    public RotataionVectorSensorEventListener(TextView outputView, TextView outputMax) {
         output = outputView;
+        outMax = outputMax;
+    }
+
+    public void onAccuracyChanged(Sensor s, int i) {
 
     }
 
-    public void onAccuracyChanged(Sensor s, int i){}
-
     public void onSensorChanged(SensorEvent se) {
         if (se.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR){
-            //float xValue = se.values[0];
-            //float yValue = se.values[1];
-            //float zValue = se.values[2];
-            String rotationVectorMessage = "The Rotation Vector Sensor Reading is:\n(" +  (String.format("%.2f", se.values[0])) + ", " + (String.format("%.2f", se.values[1])) + ", " + (String.format("%.2f", se.values[1])) + ")";
-            output.setText(rotationVectorMessage);
+            output.setText("The Rotation Vector Sensor Reading is:\n(" +  (String.format("%.2f", se.values[0])) + ", " + (String.format("%.2f", se.values[1])) + ", " + (String.format("%.2f", se.values[2])) + ")");
+            if (se.values[0] > maxX || se.values[1] > maxY || se.values[2] > maxZ) {
+                //same as above
+            }
         }
     }
 }
