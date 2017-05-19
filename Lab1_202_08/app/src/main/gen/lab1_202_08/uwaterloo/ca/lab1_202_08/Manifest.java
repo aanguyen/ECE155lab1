dc2c1f297d3077cf5f17de46df1e85d86d674a4a
janet-liu809 <janetliu11@yahoo.ca>
2017-05-19 19:35:49 -0400

    Right aligned TextViews

--- Lab1_202_08/app/src/main/gen/lab1_202_08/uwaterloo/ca/lab1_202_08/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package lab1_202_08.uwaterloo.ca.lab1_202_08;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
